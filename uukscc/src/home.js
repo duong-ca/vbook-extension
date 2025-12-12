af88e1412082028b445a875eb118e154050c0a60
duong-ca <duongsieuga@gmail.com>
2025-12-13 04:51:11 +0700

    Update home.js

--- uukscc/src/home.js
+++ uukscc/src/home.js
@@ -2,9 +2,7 @@ load('config.js');
 
 function execute() {
     return Response.success([
-        {title: "熱門小說推薦", input: "https://uukanshu.cc", script: "homecontent_fengtui.js"},
-        {title: "閱讀排行榜", input: "https://uukanshu.cc/", script: "homecontent_fengyou.js"},
-        {title: "最新小說", input: "https://uukanshu.cc", script: "homecontent_zuixin.js"},
-        {title: "最近更新", input: "https://uukanshu.cc", script: "homecontent_gengxin.js"}
+        {title: "熱門小說推薦", input: "https://uukanshu.cc/quanben/", script: "gen.js"},
     ]);
 }
+
